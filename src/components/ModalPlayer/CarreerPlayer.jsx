ba5ba6c30875f7e4a8e9cc0e288531fa27054d9e
0y0 <rigoulet.henri.pierre@gmail.com>
2022-04-25 23:05:19 +0200

    responsive

--- src/components/ModalPlayer/CarreerPlayer.jsx
+++ src/components/ModalPlayer/CarreerPlayer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CarreerPlayer = () => {
   return (
     <div className="career">
-      <p className="career-title">CAREER TITLE </p>
+      <p className="career-title">CAREER TITLES </p>
       <div className="year-titles">
         <span className="date">
           <b>2021</b> - 5
@@ -17,16 +17,6 @@ const CarreerPlayer = () => {
       <div className="year-titles">
         <span className="date">
           <b>2020</b>-4
-        </span>
-        <p>Lorem ipsum dolor sit amet, consectetur </p>
-        <p>Lorem ipsum dolor sit amet, consectetur </p>
-        <p>Lorem ipsum dolor sit amet, consectetur </p>
-        <p>Lorem ipsum dolor sit amet, consectetur </p>
-        <p>Lorem ipsum dolor sit amet, consectetur </p>
-      </div>
-      <div className="year-titles">
-        <span className="date">
-          <b>2019</b>-4
         </span>
         <p>Lorem ipsum dolor sit amet, consectetur </p>
         <p>Lorem ipsum dolor sit amet, consectetur </p>
