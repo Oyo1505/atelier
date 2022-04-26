e94e32bab28f5c68471ccf25b009decbb315a2f0
0y0 <rigoulet.henri.pierre@gmail.com>
2022-04-26 07:21:27 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./asset/css/reset.css";
-import "./index.css";
+import "./asset/css/index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { StoreProviderWrapper } from "./utils/StroreContext";
